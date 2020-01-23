8a49defba4ffaea3cdf5eb6dc2c28a3a4f716a1e
raufsl <57763124+raufsl@users.noreply.github.com>
2020-01-23 16:02:49 +0700

    Update run.php

--- run.php
+++ run.php
@@ -31,7 +31,7 @@ function change(){
         $pilihan = trim(fgets(STDIN));
         if($pilihan == "y" || $pilihan == "Y"){
         echo color("red","===========(REDEEM VOUCHER)===========");
-        echo "\n".color("yellow","!] Claim voc GOFOODBOBA07");
+        echo "\n".color("yellow","!] Claim voc PAKEGOJEK");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
@@ -44,7 +44,7 @@ function change(){
         goto goride;
         }else{
         echo "\n".color("red","-] Message: ".$message);
-        echo "\n".color("yellow","!] Claim voc GOFOODBOBA10");
+        echo "\n".color("yellow","!] Claim voc GOFOODYUK");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
